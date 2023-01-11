ef0a89f403e70d21b47a40e06270fba66ecd7a31
Wilsfried Di Majo <wilsfrieddm@gmail.com>
2023-01-11 21:57:09 +0100

    Page Contact avec Effet destructuré

---
+++ contact.js
@@ -0,0 +1,16 @@
+const effet = id => {
+   const element = document.getElementById(id),
+   text = element.innerText.split("");
+
+   element.innerText = "";
+
+   text.forEach(letter => {
+      const span = document.createElement("span");
+      span.className = "letter";
+      span.innerText = letter;
+
+      element.appendChild(span);
+   });
+}
+
+effet("separer")
